87f6d34063d0257d90b2a324d7dee9539efeeb5e
vborja21 <vborja2424@gmail.com>
2024-09-03 16:36:41 -0500

    Controller y ServiceImpl completado

--- src/main/java/com/apis/gestiontareas/apigestiontareas/controller/TareasController.java
+++ src/main/java/com/apis/gestiontareas/apigestiontareas/controller/TareasController.java
@@ -1,4 +1,100 @@
 package com.apis.gestiontareas.apigestiontareas.controller;
 
+import com.apis.gestiontareas.apigestiontareas.entity.Tareas;
+import com.apis.gestiontareas.apigestiontareas.service.TareasService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tareas")
+@AllArgsConstructor
+
 public class TareasController {
+
+    private TareasService tareasService;
+
+    @GetMapping("/listarTareas")
+
+    public ResponseEntity<List<Tareas>> listarTareas() {
+
+        return new ResponseEntity<>(tareasService.listarTareas(), HttpStatus.OK);
+
+    }
+
+    @PostMapping("/guardarTarea")
+
+    public ResponseEntity<Tareas> guardarTarea(@RequestBody Tareas tareas) {
+
+        return new ResponseEntity<>(tareasService.guardarTarea(tareas), HttpStatus.OK);
+
+    }
+
+    @PutMapping("/actualizarTarea")
+
+    public ResponseEntity<Tareas> actualizarTarea(@RequestBody Tareas tareas) {
+
+        return new ResponseEntity<>(tareasService.actualizarTarea(tareas), HttpStatus.OK);
+
+    }
+
+    @DeleteMapping("/eliminarTarea/{idTarea}")
+
+    public void eliminarTarea(@PathVariable Integer idTarea) {
+
+        tareasService.eliminarTarea(idTarea);
+
+    }
+
+    @GetMapping("/findByEstado/{estado}")
+
+    public ResponseEntity<List<Tareas>> findByEstado (@PathVariable String estado) {
+
+        return new ResponseEntity<>(tareasService.findByEstado(estado), HttpStatus.OK);
+
+    }
+
+    @GetMapping("/menorFechaVencimiento")
+
+    public ResponseEntity<List<Tareas>> menorFechaVencimiento() {
+
+        return new ResponseEntity<>(tareasService.menorFechaVencimiento(), HttpStatus.OK);
+
+    }
+
+    @GetMapping("/mayorFechaVencimiento")
+
+    public ResponseEntity<List<Tareas>> mayorFechaVencimiento() {
+
+        return new ResponseEntity<>(tareasService.mayorFechaVencimiento(), HttpStatus.OK);
+
+    }
+
+    @GetMapping("/ordenarPorTitulo")
+
+    public ResponseEntity<List<Tareas>> ordenarPorTitulo() {
+
+        return new ResponseEntity<>(tareasService.ordenarPorTitulo(), HttpStatus.OK);
+
+    }
+
+    @GetMapping("/ordenarPorDescripcion")
+
+    public ResponseEntity<List<Tareas>> ordenarPorDescripcion() {
+
+        return new ResponseEntity<>(tareasService.ordenarPorDescripcion(), HttpStatus.OK);
+
+    }
+
+    @GetMapping("/marcarComoCompletada/{idTarea}")
+
+    public ResponseEntity<Tareas> marcarComoCompletada (@PathVariable Integer idTarea) {
+
+        return new ResponseEntity<>(tareasService.marcarComoCompletada(idTarea), HttpStatus.OK);
+
+    }
+
 }
--- src/main/java/com/apis/gestiontareas/apigestiontareas/serviceImpl/TareasServiceImpl.java
+++ src/main/java/com/apis/gestiontareas/apigestiontareas/serviceImpl/TareasServiceImpl.java
@@ -1,4 +1,108 @@
 package com.apis.gestiontareas.apigestiontareas.serviceImpl;
 
-public class TareasServiceImpl {
+import com.apis.gestiontareas.apigestiontareas.entity.Tareas;
+import com.apis.gestiontareas.apigestiontareas.repository.RepositoryTareas;
+import com.apis.gestiontareas.apigestiontareas.service.TareasService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+
+public class TareasServiceImpl implements TareasService {
+
+    private RepositoryTareas repositoryTareas;
+
+    @Override
+
+    public List<Tareas> listarTareas() {
+
+        return repositoryTareas.findAll();
+
+    }
+
+    @Override
+
+    public Tareas marcarComoCompletada(Integer idTarea) {
+
+        Tareas tarea = repositoryTareas.findById(idTarea).get();
+
+        tarea.setEstado("Completada");
+
+        return repositoryTareas.save(tarea);
+
+    }
+
+    @Override
+
+    public List<Tareas> findByEstado(String estado) {
+
+        return repositoryTareas.findByEstado(estado);
+
+    }
+
+    @Override
+
+    public Tareas guardarTarea(Tareas tareas) {
+
+        return repositoryTareas.save(tareas);
+
+    }
+
+    @Override
+
+    public Tareas actualizarTarea(Tareas tareas) {
+
+        var tareaA = repositoryTareas.findById(tareas.getIdTarea()).get();
+
+        tareaA.setTitulo(tareas.getTitulo());
+        tareaA.setDescripcion(tareas.getDescripcion());
+        tareaA.setFechaVencimiento(tareas.getFechaVencimiento());
+        tareaA.setEstado(tareas.getEstado());
+
+        return repositoryTareas.save(tareaA);
+
+    }
+
+    @Override
+
+    public void eliminarTarea(Integer idTarea) {
+
+        repositoryTareas.deleteById(idTarea);
+
+    }
+
+    @Override
+
+    public List<Tareas> menorFechaVencimiento() {
+
+        return repositoryTareas.menorFechaVencimiento();
+
+    }
+
+    @Override
+
+    public List<Tareas> mayorFechaVencimiento() {
+
+        return repositoryTareas.mayorFechaVencimiento();
+
+    }
+
+    @Override
+
+    public List<Tareas> ordenarPorTitulo() {
+
+        return repositoryTareas.ordenarPorTitulo();
+
+    }
+
+    @Override
+
+    public List<Tareas> ordenarPorDescripcion() {
+
+        return repositoryTareas.ordenarPorDescripcion();
+
+    }
 }
